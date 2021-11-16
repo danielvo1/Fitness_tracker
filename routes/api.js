f7c3c0a3e96977a181203017fa7a0828def04935
Daniel Vo <daniel.vo@berkeley.edu>
2021-11-15 19:41:31 -0800

    adding a get route to retrieve workouts

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+//get to retrieve workouts 
+router.get("/api/workout", (req, res) => {
+    Transaction.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 //post to create a new workout 
 router.post("/api/workout", ({body}, res) => {
     Workout.create(body)
