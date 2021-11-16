c9aab9b5e4c4509b5f1c6641db349e47bde0f4f7
Daniel Vo <daniel.vo@berkeley.edu>
2021-11-15 21:57:15 -0800

    added changes for heroku deployment

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/boiling-thicket-11165", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
   });
   
